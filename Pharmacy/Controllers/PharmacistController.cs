29c3a17b6004560efab56e2de70349226b1ad67b
rahma-walid <62040077+rahma-walid@users.noreply.github.com>
2020-03-30 00:40:58 +0200

    delete user

---
+++ Pharmacy/Controllers/PharmacistController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pharmacy.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Web.Security;
+
+namespace Pharmacy.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
