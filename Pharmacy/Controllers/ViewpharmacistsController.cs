ac0ee94409f75d17d442ceb76432599df47b2c50
raghda-ali <raghdaali991@gmail.com>
2020-03-30 05:36:56 +0200

    add function view pharmacist

---
+++ Pharmacy/Controllers/ViewpharmacistsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pharmacy.Models;
+
+namespace Pharmacy.Controllers
+{
+    public class ViewpharmacistsController : Controller
+    {
+        private Pharmacy_DBEntities2 db = new Pharmacy_DBEntities2();
+
+        // GET: Viewpharmacists
+        public ActionResult Index()
+        {
+            return View(db.pharmacists.ToList());
+        }
+
+        // GET: Viewpharmacists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pharmacist pharmacist = db.pharmacists.Find(id);
+            if (pharmacist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacist);
+        }
+
+        // GET: Viewpharmacists/Create
+      /*  public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Viewpharmacists/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FirstName,LastName,EmailID,DateOfBirth,Password,IsEmailVertified,ActivationCode")] pharmacist pharmacist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pharmacists.Add(pharmacist);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pharmacist);
+        }
+
+        // GET: Viewpharmacists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pharmacist pharmacist = db.pharmacists.Find(id);
+            if (pharmacist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacist);
+        }
+
+        // POST: Viewpharmacists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,FirstName,LastName,EmailID,DateOfBirth,Password,IsEmailVertified,ActivationCode")] pharmacist pharmacist)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pharmacist).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pharmacist);
+        }
+
+        // GET: Viewpharmacists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pharmacist pharmacist = db.pharmacists.Find(id);
+            if (pharmacist == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pharmacist);
+        }
+
+        // POST: Viewpharmacists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pharmacist pharmacist = db.pharmacists.Find(id);
+            db.pharmacists.Remove(pharmacist);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }*/
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
